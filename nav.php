92cc9c48a1535e403092abab0e5c2a109b40640b
Ankitharai-78 <70069375+Ankitharai-78@users.noreply.github.com>
2021-07-14 01:36:42 +0530

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php"></a>
+		<div class="container">
+			<a class="navbar-brand" href="#">GRIP</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="https://localhost/Banking_system/index.php">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="https://localhost/Banking_system/createuser.php">Create User</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="https://localhost/Banking_system/transfermoney.php">Make Transaction</a>
+					</li>
+          <li class="nav-item active">
+						<a class="nav-link" href="https://localhost/Banking_system/transactionhistory.php">Transaction History</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
